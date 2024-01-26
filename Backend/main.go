1b0aedacc7cbcdfb6df7ea0c3fe89bb0bc36dcc5
hreamer <reamer.hudson@gmail.com>
2024-01-26 16:46:42 -0500

    Added basic hello world server functionality

--- Backend/main.go
+++ Backend/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
+
+// Function takes in a ResponseWriter and the incoming request
+// Function is intended to be used to serve all "/" or root requests
+func getRoot(writer http.ResponseWriter, request *http.Request) {
+
+	fmt.Println("Received \"/\" request")
+
+	io.WriteString(writer, "<h1> Hello World <h1>")
+
+	fmt.Println("Root request served")
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	http.HandleFunc("/", getRoot)
+
+	http.ListenAndServe(":3333", nil)
 }
