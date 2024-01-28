6cc4df387aad0d8c58feb558ce4684e984ad803c
hreamer <reamer.hudson@gmail.com>
2024-01-28 14:32:55 -0500

    Boiler plate code for backend

--- Backend/main.go
+++ Backend/main.go
@@ -12,15 +12,24 @@ func getRoot(writer http.ResponseWriter, request *http.Request) {
 
 	fmt.Println("Received \"/\" request")
 
-	//Write to the packet
+	//Write to the response packet
 	io.WriteString(writer, "<h1> Hello World <h1>")
 
 	fmt.Println("Root request served")
 }
 
+func template1(writer http.ResponseWriter, request *http.Request) {
+	//step 1 unmarshall the JSON into the template info struct
+
+	//step 2 use the struct to template the LaTeX
+
+	//write the templated LaTeX to the response packet
+}
+
 func main() {
 	//HTTP server handler functions
 	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/template1", template1)
 
 	//Start the server on the desired PORT
 	http.ListenAndServe(":3333", nil)
