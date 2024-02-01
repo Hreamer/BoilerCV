c757cb3829980a3ab8ea9f2b71297e45fe9a3796
ehlacy01 <ellis.h.lacy@gmail.com>
2024-01-31 20:34:36 -0500

    Initial Front End Commit

---
+++ Frontend/boiler-cv/src/Components/Landing.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={styles.container}>
+      <div style={styles.square}>
+        <p>Hello World!</p>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '200px',
+  },
+  square: {
+    width: '200px',
+    height: '200px',
+    backgroundColor: 'lightblue',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+
+export default Landing;
