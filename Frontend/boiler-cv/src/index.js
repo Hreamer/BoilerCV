ac587bb0a4ca6f9c6b9d23595e006cd50360db6e
ehlacy01 <ellis.h.lacy@gmail.com>
2024-01-31 20:37:01 -0500

    Updates file paths

--- Frontend/boiler-cv/src/index.js
+++ Frontend/boiler-cv/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Landing from './Components/Landing';
+import Landing from './Components/Landing/Landing';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
