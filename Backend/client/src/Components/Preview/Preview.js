3d5ee7647f39f279e28662895d1f030eef659110
ecloev <ecloev@outlook.com>
2024-02-05 02:29:24 -0500

    Preview

--- Backend/client/src/Components/Preview/Preview.js
+++ Backend/client/src/Components/Preview/Preview.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Preview.css";
+import pdfSample from "./dummy.pdf";
 
 const Preview = () => {
-  return <div className="preview"><h1>Preview</h1></div>;
+  return (
+    <div className="preview">
+      <h1>Preview</h1>
+      <iframe src={pdfSample} className="previewWindow" title="PDF Viewer"/>
+    </div>
+  );
 };
 
 export default Preview;
