7e915b413084ec37ca46b6fed177c9e048e0f487
ecloev <ecloev@outlook.com>
2024-02-05 02:30:26 -0500

    Change to target from sample

--- Backend/client/src/Components/Preview/Preview.js
+++ Backend/client/src/Components/Preview/Preview.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Preview.css";
-import pdfSample from "./dummy.pdf";
+import targetPDF from "./dummy.pdf";
 
 const Preview = () => {
   return (
     <div className="preview">
       <h1>Preview</h1>
-      <iframe src={pdfSample} className="previewWindow" title="PDF Viewer"/>
+      <iframe src={targetPDF} className="previewWindow" title="PDF Viewer"/>
     </div>
   );
 };
