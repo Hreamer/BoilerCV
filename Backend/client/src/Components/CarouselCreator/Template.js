01224083c137a67fa344a1729f6b3ed5e1bd099e
ecloev <ecloev@outlook.com>
2024-02-14 13:12:57 -0500

    Temporary error message for resume creation

--- Backend/client/src/Components/CarouselCreator/Template.js
+++ Backend/client/src/Components/CarouselCreator/Template.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./Template.css";
 
 const Template = ({ imageUrl, templateName }) => {
+  const tempFix = () => {
+    alert("Resume creation is not current enabled!");
+  }
   return (
     <div className="card">
       <img className="product-image" src={imageUrl} alt={templateName} />
       <h3>{templateName}</h3>
       <p>
-        <button>Create</button>
+        <button onClick={tempFix}>Create</button>
       </p>
     </div>
   );
