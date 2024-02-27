2bdd6ad16a5de57f0a519be5aa3f8825a2db16b4
hreamer <reamer.hudson@gmail.com>
2024-02-27 12:56:48 -0500

    Ok a bit more Templating work

--- Backend/templater.go
+++ Backend/templater.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"text/template"
 )
 
@@ -31,11 +34,32 @@ type Template1Info struct {
 var tmpl1 *template.Template
 
 func createTemplate1(writer http.ResponseWriter, request *http.Request) {
+	var tmplInfo Template1Info
+
 	//Parse the JSON
+	err := json.NewDecoder(request.Body).Decode(&tmplInfo)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	//Check that username is initiated in struct
+	//This is needed cause without the username we can't upload to the DB
+	//Alternatively we could push this check back to the db step and skip the db step if it is missing
+	if tmplInfo.Username == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Println("Username was not present after parsing")
+		return
+	}
 
 	//Use struct to fill template
+	//For now we will use Std Out to verify results but this can and should be changed at a later date
+	err = tmpl1.Execute(os.Stdout, tmplInfo)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Println("\nError Executing Template 1")
+		return
+	}
 
 	//Take the result and turn to PDF
 
