e2c07d02d7ef40356bc6c80ad4761f9b6a730d30
ecloev <ecloev@outlook.com>
2024-03-07 10:40:18 -0500

    Rename and delete buttons

--- Backend/client/src/Components/MyResumes/MyResumes.js
+++ Backend/client/src/Components/MyResumes/MyResumes.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./MyResumes.css";
 import ResumeCard from "./ResumeCard";
 
+const onRename = (resumeID) => {
+  alert("Rename!");
+};
+
+const onDelete = (resumeID) => {
+  alert("Delete!");
+};
+
 const MyResumes = ({ onOpenResume }) => {
   return (
     <div className="my-resumes">
@@ -11,11 +19,15 @@ const MyResumes = ({ onOpenResume }) => {
           imageUrl="https://d25zcttzf44i59.cloudfront.net/academic-word-resume-template.png"
           resumeName="Resume 1"
           onOpen={() => onOpenResume("1")}
+          onRename={() => onRename()}
+          onDelete={() => onDelete()}
         />
         <ResumeCard
           imageUrl="https://d25zcttzf44i59.cloudfront.net/academic-word-resume-template.png"
           resumeName="Resume 2"
           onOpen={() => onOpenResume("2")}
+          onRename={() => onRename()}
+          onDelete={() => onDelete()}
         />
       </div>
     </div>
