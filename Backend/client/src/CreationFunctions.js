4920a47aa7798f75a3942a34257c57924fb29be1
ehlacy01 <70302106+ehlacy01@users.noreply.github.com>
2024-03-23 15:50:17 -0400

    Ehlacy: Resume List Updates correctly (#29)
    * Added more to template file
    
    * Dummy data in resume list
    
    * Resume List now updates correctly

--- Backend/client/src/CreationFunctions.js
+++ Backend/client/src/CreationFunctions.js
@@ -24,3 +24,27 @@ const handleUpdate = () => {
        
     });
 }; 
+
+const populateResumeFields = (templateID) => {
+    const username = localStorage.getItem("username");
+    const resumeName = templateID;
+    fetch("http://localhost:3333/getResumeInfo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, resumeName }),
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+    }).then((resumeData) => {
+        for (const key in resumeData) {
+          
+        }
+    }).catch((error) => {
+        // If there's a network error, set the error state
+         
+    });
+}
