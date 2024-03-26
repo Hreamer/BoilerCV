9e51a32b998cd56015c38f5cd002d70e46e69be7
ehlacy01 <70302106+ehlacy01@users.noreply.github.com>
2024-03-25 22:56:10 -0400

    Ehlacy (#31)
    * Added more to template file
    
    * Dummy data in resume list
    
    * Resume List now updates correctly
    
    * small tex changes
    
    * fixed template.go
    
    * Base Level, still needs handleUpdate sharing

---
+++ Backend/client/src/Components/CreationFunctions.js
@@ -0,0 +1,56 @@
+import PersonalInformation from './CreationWindow/Sections/PersonalInformation';
+
+const CreationFunctions = () => {
+  const handleUpdate = () => {
+      const Username = localStorage.getItem("username");
+      const ResumeName = localStorage.getItem("templateID");
+      console.log(newPersonalData);
+      
+      fetch("http://localhost:3333/updatePreview", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ Username, ResumeName }),
+      })
+      .then((response) => {
+          if (response.ok) {
+            // If the response is successful
+            
+          } else {
+            // If there's an error response, set the error state
+            
+          }
+        })
+        .catch((error) => {
+          // If there's a network error, set the error state
+          
+      });
+  };
+
+  const populateResumeFields = (templateID) => {
+      const username = localStorage.getItem("username");
+      const resumeName = templateID;
+      fetch("http://localhost:3333/getResumeInfo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, resumeName }),
+      }).then((response) => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json(); 
+      }).then((resumeData) => {
+          for (const key in resumeData) {
+            
+          }
+      }).catch((error) => {
+          // If there's a network error, set the error state
+          
+      });
+  }
+}
+
+export default CreationFunctions;
