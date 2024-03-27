6423db4b0e918bd996254522b190c564b9e61976
ehlacy01 <ellis.h.lacy@gmail.com>
2024-03-27 12:37:32 -0400

    add usestate

--- Backend/client/src/Components/Preview/Preview.js
+++ Backend/client/src/Components/Preview/Preview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Preview.css";
 import ExportLatex from "../ExportLatex/ExportLatex";
 import ExportDocx from "../ExportDocx/ExportDocx";
